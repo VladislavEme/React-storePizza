decf05327cc377e8ebfb6b8dddc8a8d34c1b2bb6
Vlad Em <@haos1996haos@gmail.com>
2022-07-08 16:37:58 +0300

    add components

---
+++ 01_first_project/my-app-1/src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="header-logo"
+        src="https://image.winudf.com/v2/image1/Y29tLndNZXNzZW5nZXJfODAzOTA4MV9pY29uXzE1NDI0OTM1MTJfMDM3/icon.png?w=340&fakeurl=1"
+        alt="Логотип"
+      />
+    </header>
+  );
+};
+
+export default Header;
